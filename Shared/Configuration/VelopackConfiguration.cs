4e97ffc9a6e26c4c1f7f72d702413c496cd00d85
yassin <yassin.ajanif@gmail.com>
2026-09-04 20:29:09 +0200

    Update GitHub repository URL in VelopackConfiguration to reflect new project location

--- Shared/Configuration/VelopackConfiguration.cs
+++ Shared/Configuration/VelopackConfiguration.cs
@@ -4,5 +4,5 @@ public static class VelopackConfiguration
 {
     public const string PackId = "Sonlighting.GestionCommerciale";
     public const string MainExe = "GestionCommerciale.exe";
-    public const string GitHubRepoUrl = "https://github.com/yassin-ajanif/sonlightining";
+    public const string GitHubRepoUrl = "https://github.com/yassin-ajanif/SAHRAOUI";
 }
